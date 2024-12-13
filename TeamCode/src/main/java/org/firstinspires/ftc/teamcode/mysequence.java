6b27e181241fba2e583fdde856ad629bdc9c0735
David <Goldhamerda@denveracademy.org>
2024-12-13 15:10:57 -0700

    AHAHSDAJMShfafkhxnkjhbGnKJANKBFUhmxngBKJngjUJHMVYGJHGbtJNkgBgyjFvjyhbgUTYGtyfhjtyfgytFGDEPRESOIN

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mysequence.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mysequence.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 /*
  * This is an example of a more complex path to really test the tuning.
@@ -21,30 +22,18 @@ public class mysequence extends LinearOpMode {
 
         if (isStopRequested()) return;
 
-        Trajectory traj = drive.trajectoryBuilder(new Pose2d())
-                .splineTo(new Vector2d(-24.86, -48.93), Math.toRadians(154.58))
-                .splineTo(new Vector2d(43.36, -36.00), Math.toRadians(10.73))
-                .splineTo(new Vector2d(35.40, 2.98), Math.toRadians(101.53))
-                .splineTo(new Vector2d(9.75, 49.72), Math.toRadians(118.76))
-                .splineTo(new Vector2d(-51.71, 30.03), Math.toRadians(197.76))
-                .splineTo(new Vector2d(-39.58, -25.06), Math.toRadians(-77.58))
-                .splineTo(new Vector2d(6.56, -0.20), Math.toRadians(28.32))
-                .splineTo(new Vector2d(-5.37, 29.44), Math.toRadians(170.36))
-                .splineTo(new Vector2d(-37.99, 14.92), Math.toRadians(161.26))
-                .splineTo(new Vector2d(-51.12, -28.44), Math.toRadians(-35.24))
-                .splineTo(new Vector2d(45.35, -52.71), Math.toRadians(-39.06))
-                .splineTo(new Vector2d(22.67, -44.75), Math.toRadians(160.67))
-                .splineTo(new Vector2d(20.09, -63.85), Math.toRadians(262.29))
+        Trajectory traj0 = drive.trajectoryBuilder(new Pose2d())
+                .splineTo(new Vector2d(-0.20, -39.98), Math.toRadians(134.44))
+                .splineTo(new Vector2d(-17.90, -36.00), Math.toRadians(9180.00))
+                .splineTo(new Vector2d(-27.25, -35.20), Math.toRadians(152.74))
+                .splineTo(new Vector2d(-34.81, -26.85), Math.toRadians(120.00))
+                .splineTo(new Vector2d(-38.78, -16.91), Math.toRadians(112.01))
+                .splineTo(new Vector2d(-46.74, -22.48), Math.toRadians(263.02))
+                .splineTo(new Vector2d(-47.73, -47.34), Math.toRadians(-86.66))
+                .splineTo(new Vector2d(-58.48, -63.45), Math.toRadians(230.52))
                 .build();
 
-        drive.followTrajectory(traj);
 
-        sleep(2000);
-
-        drive.followTrajectory(
-                drive.trajectoryBuilder(traj.end(), true)
-                        .splineTo(new Vector2d(0, 0), Math.toRadians(180))
-                        .build()
-        );
+        drive.followTrajectory(traj0);
     }
 }
